05de4eb5423112844c700a1b7320bd9d3f8a88dd
Benson Zheng <benson515@sbcglobal.net>
2019-01-03 20:25:29 -0800

    some setup

---
+++ pythonbasic.py
@@ -0,0 +1,58 @@
+# WELCOME TO THE PRACTICE
+
+# === PYTHON BASICS ===
+# === PYTHON BASICS ===
+
+# i have variables set up for practice
+number1 = 26 		#int
+number2 = "26"  	#string
+number3 = 26.0  	#float 
+list1 = [1,2,3,4,5] #list of int
+list2 = ['a', 'b', 'c', 'd'] #list of char, because of single quotes ''
+list3 = ["e", "f", "g", "h"] #list of string, because of double quotes ""
+
+# So in order to use the function print() what you pass in must be the same type int or string, 
+
+# python provides functions to convert variables between types str() and int()
+# str(x) turns x into a string
+# int(x) turns x into a integer if it can , int("hello") wouldnt work
+# you can add lists together without converting
+
+# lists has indexs to access the elemnts in it starting from 0
+# list2[0] would equals the first elemnt which is 'a' 
+# list2[2] would equal 'c'
+# you can also get the length of the list using len(list1), which would get us 5
+
+# 1. print number1 and number2 on the same line as int
+# 2. print number1 and number2 on the same line as string
+# 3. print list1 
+# 4. print list2 and list3 on the same line
+# 5. print the length of list2 
+# 6. print the first and last element of list1, you should get 1 and 5
+
+# 7. assign your name and age to the variables and print it in the format
+# My name is {yourname} and I'm age {yourage}
+yourname = ""
+yourage = ""
+
+# example.
+print("FIRST PART PRACTICE")
+# 1.
+
+# 2.
+
+# 3. 
+
+# 4. 
+
+# 5.
+
+# 6.
+
+# 7. 
+
+# END OF FIRST PART 
+
+
+
+
---
+++ pythonloops.py
@@ -0,0 +1,51 @@
+# === PYTHON LOOPS ===
+# === PYTHON LOOPS ===
+
+
+list = [1,2,3,4,5,6,7,8,9]
+
+# basic loop structure
+
+# for i in list:
+# 	print(i)
+
+# the for loop translates to... 
+# for each element i in list:
+# do something, in this case print(i)
+
+# for i in range(0, len(list)-1 ):
+	# print(list[i])
+# this is also a loop except we choose where to start using the index
+# to chose which element to print
+
+# we can throw in a if statement to only print or do something IF something is true
+# lets say we want to print odd numberss only how do we test that?
+# num = 10 % 2 ,num would = 0,  % gives us the remainder after division (10 divided by 2 remainder = 0)  
+# so a num is odd when a number % 2 is not 0
+# if(num % 2 == 0 ) what do we have?
+
+
+# === PRACTICE ===
+# 1. print all the elements from the list
+# 2. print the first 3 elements from list
+# 3. print the last 3 elements from the list
+# 4. print only the even elements of the list
+# 5. sum up the numbers of the list 
+# 6. sum up all the odd numbers of the list
+
+# === start here ===
+print("python loops start here!")
+# 1.
+
+# 2.
+
+# 3.
+
+# 4.
+
+# 5.
+
+# 6.
+
+
+
